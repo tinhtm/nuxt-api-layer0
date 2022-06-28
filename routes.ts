23dac7f9bb684f605231bd30aa7b3c998bfe4e56
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-06-28 14:37:01 +0530

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -1,50 +1,57 @@
-import { Router } from '@layer0/core'
-import { nuxtRoutes } from '@layer0/nuxt-nitro'
+import { Router } from '@layer0/core';
+import { nuxtRoutes } from '@layer0/nuxt-nitro';
 
-export default new Router()
-.get('/category/all-products', ({ cache }) => {
-    cache({
-      edge: { maxAgeSeconds: 60 * 60, staleWhileRevalidateSeconds: 60 * 60 },
-    })    
-})
-.get('/api/products', ({ cache, proxy }) => {
-    cache({
-        edge: {
-            maxAgeSeconds: 60 * 60 * 24,           // keep entries in the cache for 24 hours
-            staleWhileRevalidateSeconds: 60 * 60  // when a cached page is older than 24 hours, serve it one more time
-            // for up to 60 minutes while fetching a new version from the origin
-        }
-    })
-    proxy('products_dev',{ path: '/api/products' })
-})
-.get('/api/products/product-types', ({ cache, proxy }) => {
-    cache({
-        edge: {
-            maxAgeSeconds: 60 * 60 * 24,           // keep entries in the cache for 24 hours
-            staleWhileRevalidateSeconds: 60 * 60  // when a cached page is older than 24 hours, serve it one more time
-            // for up to 60 minutes while fetching a new version from the origin
-        }
-    })
-    proxy('products_dev')
-})
-.get('/api/products/health-concerns', ({ cache, proxy }) => {
-    cache({
-        edge: {
-            maxAgeSeconds: 60 * 60 * 24,           // keep entries in the cache for 24 hours
-            staleWhileRevalidateSeconds: 60 * 60  // when a cached page is older than 24 hours, serve it one more time
-            // for up to 60 minutes while fetching a new version from the origin
-        }
-    })
-    proxy('products_dev')
-})
-.get('/api/products/diet-types', ({ cache, proxy }) => {
-    cache({
-        edge: {
-            maxAgeSeconds: 60 * 60 * 24,           // keep entries in the cache for 24 hours
-            staleWhileRevalidateSeconds: 60 * 60  // when a cached page is older than 24 hours, serve it one more time
-            // for up to 60 minutes while fetching a new version from the origin
-        }
-    })
-    proxy('products_dev')
-})
-.use(nuxtRoutes)
+const routesToCache: any = [
+  {
+    cacheType: {
+      edge: {
+        maxAgeSeconds: 60 * 60 * 24, // keep entries in the cache for 24 hours
+        staleWhileRevalidateSeconds: 60 * 60, // when a cached page is older than 24 hours, serve it one more time
+        // for up to 60 minutes while fetching a new version from the origin
+      },
+      browser: false,
+    },
+    routesThatShouldFollowThisCache: [
+      '/api/products',
+      '/api/products/product-types',
+      '/api/products/health-concerns',
+      '/api/products/diet-types',
+    ],
+    proxyingTo: 'products_dev',
+  },
+  {
+    cacheType: {
+      edge: false,
+      browser: false,
+    },
+    routesThatShouldFollowThisCache: ['/api/:path*'],
+    proxyingTo: 'products_dev',
+  },
+  {
+    cacheType: {
+      edge: {
+        maxAgeSeconds: 60 * 60,
+        staleWhileRevalidateSeconds: 60 * 60,
+      },
+    },
+    routesThatShouldFollowThisCache: ['/category/all-products'],
+  },
+];
+
+const router = new Router();
+
+routesToCache.forEach((i) => {
+  i.routesThatShouldFollowThisCache.forEach((route) => {
+    router.match(route, ({ cache, proxy }) => {
+      cache(i.cacheType);
+      if (i?.proxyingTo) {
+        // Falls back to Nuxt if proxy is not set
+        proxy(i.proxyingTo);
+      }
+    });
+  });
+});
+
+router.use(nuxtRoutes);
+
+export default router;
